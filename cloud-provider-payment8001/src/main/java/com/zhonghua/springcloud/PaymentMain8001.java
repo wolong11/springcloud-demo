60ed2f8ffcda1a643f9d5bedf9e656f33587f54d
zhonghua <664273805@qq.com>
2020-03-16 14:42:45 +0800

    分布式微服务学习demo

---
+++ cloud-provider-payment8001/src/main/java/com/zhonghua/springcloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.zhonghua.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: ZhongHua
+ * @Description:
+ * @Date: Created in 22:05 2020/3/15
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
